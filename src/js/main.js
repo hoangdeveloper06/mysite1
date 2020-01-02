38375f5e17276bfce4a981f1cb7f1a18bee3b1d5
hoangdeveloper06 <56692700+hoangdeveloper06@users.noreply.github.com>
2020-01-02 21:16:39 +0700

    aaa

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,32 @@ import "@babel/polyfill";
 import U from "./lib/Utilities";
 import Loading from "./lib/Loading";
 
+function HomeBanner() {
+    var mySwiper = new Swiper('.banner .swiper-container', {
+        spaceBetween: 30,
+        effect: 'fade',
+        fadeEffect: {
+            slowFade: true,
+        },
+        autoplay: {
+            delay: 3000,
+            out: 1500,
+        },
+        slidesPerView: 1
+    });
+}
+
+//Hamberger_menu
+function activeMenu() {
+    $('.hamberger-menu').on('click', function() {
+        $(this).find('.bar').toggleClass('change');
+        $('body').toggleClass('noscroll');
+        $(this).siblings('.mobile-menu').toggleClass('show-menu');
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-	Loading();
+    Loading();
+    HomeBanner();
+    activeMenu();
 });
