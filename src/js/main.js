afbaab8c041cc67fab87f85b79268cfff9d4222b
hoangdeveloper06 <hoangdeveloper06@gmail.com>
2020-01-06 18:04:02 +0700

    a

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import "@babel/polyfill";
 import U from "./lib/Utilities";
 import Loading from "./lib/Loading";
 
+
 //smooth scroll
 function stickyHeader(){
     $(window).scroll(function() {
@@ -50,42 +51,42 @@ function secSlide() {
 //Hamberger_menu
 function activeMenu() {
     $('.hamberger-menu').on('click', function () {
-        $(this).find('.bar').toggleClass('change');
-        $('body').toggleClass('noscroll');
-        $(this).siblings('.mobile-menu').toggleClass('show-menu');
+        if ($(this).on('clicked')) {
+            $(this).find('.bar').toggleClass('change');
+            $('body').toggleClass('noscroll');
+            $(this).siblings('.mobile-menu').toggleClass('show-menu');
+        }
     });
 }
 //gotop
 function scrolltop() {
     var id_button = '#gotoTop';
-
-    // Kéo xuống cách 300
     var offset = 300;
-
-    // THời gian trượt 0.5 giây
     var duration = 500;
-
-    // kiểm tra sự ẩn hiện của button
-    jQuery(window).scroll(function () {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery(id_button).fadeIn(duration);
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > offset) {
+            $(id_button).fadeIn(duration);
         } else {
-            jQuery(id_button).fadeOut(duration);
+            $(id_button).fadeOut(duration);
         }
     });
-
-    // trượt lên top
-    jQuery(id_button).click(function (event) {
+    $(id_button).click(function (event) {
         event.preventDefault();
-        jQuery('html, body').animate({ scrollTop: 0 }, duration);
+        $('html, body').animate({ scrollTop: 0 }, duration);
         return false;
     });
 }
-
-// Thực hiện
 $(document).ready(function () {
     scrolltop();
 });
+
+// function closeNav() {
+//     $('a').on('click', function (){
+//         $(".mobile-menu").toggleClass("hide");
+//     });
+//   }
+
+
 document.addEventListener('DOMContentLoaded', () => {
     Loading();
     HomeBanner();
@@ -93,4 +94,5 @@ document.addEventListener('DOMContentLoaded', () => {
     secSlide();
     scrolltop();
     stickyHeader();
+    // closeNav()
 });
